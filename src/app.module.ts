724b1530b2e600191af14da4ada7fb6f9eb8c2f0
Brian <patinobrian@gmail.com>
2023-07-05 10:48:22 -0400

    configure logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      ignoreEnvFile: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [Logger, AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
+import 'dotenv/config'; // must be first import
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ElasticPinoLogger } from './elasticpino.logger';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const pinoLogger = new ElasticPinoLogger();
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      logger: pinoLogger.logger,
+    }),
+    {
+      logger: pinoLogger,
+    },
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Badge Buddy API')
+    .setDescription('The Badge Buddy API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  // accept connections to other hosts
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
