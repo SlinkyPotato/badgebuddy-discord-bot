95e7c1e612d720ce123be90200fe311d5ab89d99
SlinkyPotato <patinobrian@gmail.com>
2023-07-06 13:38:44 -0400

    migrate /poap help

--- src/help/help.command.ts
+++ src/help/help.command.ts
@@ -1,13 +1,9 @@
-import { Command, Handler } from '@discord-nestjs/core';
-import { ContextMenuCommandInteraction } from 'discord.js';
+import { Command, UseGroup } from '@discord-nestjs/core';
+import { PoapSubCommand } from './poap/poap-sub.command';
 
 @Command({
   name: 'help',
   description: 'Additional information on the POAP distribution commands.',
+  include: [PoapSubCommand],
 })
-export class HelpCommand {
-  @Handler()
-  onHelpCommand(interaction: ContextMenuCommandInteraction) {
-    return 'help command response ';
-  }
-}
+export class HelpCommand {}
--- src/help/help.module.ts
+++ src/help/help.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DiscordModule } from '@discord-nestjs/core';
 import { HelpCommand } from './help.command';
+import { PoapSubCommand } from './poap/poap-sub.command';
 
 @Module({
   imports: [DiscordModule.forFeature()],
-  providers: [HelpCommand],
+  providers: [HelpCommand, PoapSubCommand],
 })
 export class HelpModule {}
