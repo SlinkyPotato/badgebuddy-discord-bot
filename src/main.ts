8d44e17dd7bac32c07c9c44c47d7202879276c5c
Brian Patino <Patinobrian@gmail.com>
2023-11-03 20:59:13 -0400

    refactor: simplify main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,16 @@
 import 'dotenv/config';
-import bootstrap from './bootstrap';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {
+  CommonPinoLogger,
+  CommonPinoLoggerService,
+} from '@solidchain/badge-buddy-common';
+
+async function bootstrap() {
+  const pinoLogger = new CommonPinoLogger('discord-bot');
+  const pinoLoggerService = new CommonPinoLoggerService(pinoLogger);
+  await NestFactory.createApplicationContext(AppModule, {
+    logger: pinoLoggerService,
+  });
+}
 bootstrap();
