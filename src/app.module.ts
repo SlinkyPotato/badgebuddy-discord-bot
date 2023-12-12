67d239dd4ba84ba7de5eca3cebadd3899db70774
Brian Patino <Patinobrian@gmail.com>
2023-12-11 23:38:54 -0500

    wip

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,17 @@ import { EndEventCommandModule } from './end-event-command/end-event-command.mod
 import { ClaimPoapCommandModule } from './claim-poap-command/claim-poap-command.module';
 import { DistributePoapCommandModule } from './distribute-poap-command/distribute-poap-command.module';
 import { HelpCommandModule } from './help-command/help-command.module';
+import Joi from 'joi';
 
 @Module({
   imports: [
-    CommonConfigModule.forRoot(),
+    CommonConfigModule.forRoot({
+      validationSchema: {
+        REDIS_HOST: Joi.string().required(),
+        REDIS_PORT: Joi.number().required(),
+        REDIS_CACHE_MIN: Joi.number().required(),
+      }
+    }),
     DiscordConfigModule.forRootAsync(),
     StartEventCommandModule,
     EndEventCommandModule,
