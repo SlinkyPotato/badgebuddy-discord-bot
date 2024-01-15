2f51c6bfa197efafddddb2050cb63e50f25f4339
SlinkyPotato <patinobrian@gmail.com>
2024-01-15 12:15:15 -0500

    fix tests

--- src/api-badgebuddy/poaps-api/poaps-api.service.spec.ts
+++ src/api-badgebuddy/poaps-api/poaps-api.service.spec.ts
@@ -1,13 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PoapsApiService } from './poaps-api.service';
-import { describe, beforeEach, it, expect } from '@jest/globals';
+import { describe, beforeEach, jest, it, expect } from '@jest/globals';
+import { Logger } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { ConfigService } from '@nestjs/config';
 
 describe('PoapsApiService', () => {
   let service: PoapsApiService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PoapsApiService],
+      providers: [
+        PoapsApiService,
+        {
+          provide: Logger,
+          useValue: jest.fn(),
+        },
+        { provide: HttpService, useValue: jest.fn() },
+        { provide: ConfigService, useValue: jest.fn() },
+      ],
     }).compile();
 
     service = module.get<PoapsApiService>(PoapsApiService);
--- src/claim-poap-command/claim-poap-command.service.spec.ts
+++ src/claim-poap-command/claim-poap-command.service.spec.ts
@@ -1,13 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ClaimPoapCommandService } from './claim-poap-command.service';
-import { describe, beforeEach, it, expect } from '@jest/globals';
+import { describe, beforeEach, it, expect, jest } from '@jest/globals';
+import { Logger } from '@nestjs/common';
 
 describe('ClaimPoapCommandService', () => {
   let service: ClaimPoapCommandService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ClaimPoapCommandService],
+      providers: [
+        ClaimPoapCommandService,
+        {
+          provide: Logger,
+          useValue: jest.fn(),
+        },
+      ],
     }).compile();
 
     service = module.get<ClaimPoapCommandService>(ClaimPoapCommandService);
